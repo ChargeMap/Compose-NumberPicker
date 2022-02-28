e5e78f2e80cce5c6a105c44b157ff721337a2dab
Raphaël <raphael@chargemap.com>
2022-02-28 12:12:35 +0100

    update readme

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -5,7 +5,18 @@ plugins {
 }
 
 android {
-    kotlinOptions.jvmTarget = "11"
+    // JAVA 11
+    compileOptions {
+        sourceCompatibility = org.gradle.api.JavaVersion.VERSION_11
+        targetCompatibility = org.gradle.api.JavaVersion.VERSION_11
+    }
+
+    project.tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = JavaVersion.VERSION_11.toString()
+        }
+    }
+
     buildFeatures.compose = true
 
     composeOptions {
@@ -16,9 +27,9 @@ android {
 dependencies {
     implementation(project(":lib"))
 
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("androidx.activity:activity-compose:1.3.1")
+    implementation("com.google.android.material:material:1.5.0")
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("androidx.activity:activity-compose:1.4.0")
 
-    testImplementation("junit:junit:4.13.1")
+    testImplementation("junit:junit:4.13.2")
 }

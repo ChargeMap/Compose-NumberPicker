5c2462a6d64b8ef031936ecd5ae05e661ee519b9
Christian <cjrcodes@gmail.com>
2022-03-26 21:39:35 -0400

    Added leadingZero (leading zero) boolean parameter to NumberPicker (gradle file from original project showing errors, needs fixing)

--- sample/src/main/kotlin/com/chargemap/android/sample/PreviewComponents.kt
+++ sample/src/main/kotlin/com/chargemap/android/sample/PreviewComponents.kt
@@ -1,75 +1,44 @@
-package com.chargemap.android.sample
-
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.chargemap.compose.numberpicker.*
 
+
+@Preview
 @Composable
-fun MainActivityUI() {
-
-    val scrollState = rememberScrollState()
-
-    MaterialTheme {
-        Scaffold(
-            topBar = {
-                TopAppBar(title = { Text(stringResource(id = R.string.app_name)) })
-            }
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .verticalScroll(scrollState)
-            ) {
-                Column(
-                    Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-                ) {
-                    NumberPicker()
-                    HoursNumberPicker1()
-                    HoursNumberPicker2()
-                    HoursNumberPicker3()
-                    HoursNumberPicker4()
-                    DoublesPicker()
-                    FruitPicker()
-                    IntRangePicker()
-                }
-            }
-        }
-    }
-}
-
-@Composable
-private fun NumberPicker() {
+private fun NumberPickerPreview() {
     var state by remember { mutableStateOf(0) }
-    NumberPicker(
+    com.chargemap.compose.numberpicker.NumberPicker(
         leadingZero = true,
         value = state,
         range = 0..10,
         onValueChange = {
             state = it
-        }
+        },
+                textStyle = TextStyle(Color.White)
+
     )
 }
 
+@Preview
 @Composable
-private fun HoursNumberPicker1() {
-    var state by remember { mutableStateOf<Hours>(FullHours(12, 43)) }
+private fun HoursNumberPicker1Preview() {
+    var state by remember { mutableStateOf<Hours>(FullHours(12, 0)) }
     HoursNumberPicker(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 16.dp),        leadingZero = true,
-
+            .padding(vertical = 16.dp),
+        leadingZero = true,
         dividersColor = MaterialTheme.colors.error,
         value = state,
         onValueChange = {
@@ -81,18 +50,20 @@ private fun HoursNumberPicker1() {
                 textAlign = TextAlign.Center,
                 text = ":"
             )
-        }
+        },
+    textStyle = TextStyle(Color.White)
     )
 }
 
+@Preview
 @Composable
-private fun HoursNumberPicker2() {
-    var state by remember { mutableStateOf<Hours>(AMPMHours(9, 43, AMPMHours.DayTime.PM)) }
+private fun HoursNumberPicker2Preview() {
+    var state by remember { mutableStateOf<Hours>(AMPMHours(9, 0, AMPMHours.DayTime.PM)) }
     HoursNumberPicker(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 16.dp),        leadingZero = true,
-
+            .padding(vertical = 16.dp),
+        leadingZero = true,
         dividersColor = MaterialTheme.colors.secondary,
         value = state,
         onValueChange = {
@@ -109,23 +80,26 @@ private fun HoursNumberPicker2() {
             Spacer(
                 modifier = Modifier.size(24.dp),
             )
-        }
+        },
+        textStyle = TextStyle(Color.White)
     )
 }
 
+@Preview
 @Composable
-private fun HoursNumberPicker3() {
+private fun HoursNumberPicker3Preview() {
     var state by remember { mutableStateOf<Hours>(FullHours(9, 20)) }
 
     HoursNumberPicker(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 16.dp),        leadingZero = true,
-
+            .padding(vertical = 16.dp),
         value = state,
         onValueChange = {
             state = it
         },
+        leadingZero = true,
+
         minutesRange = IntProgression.fromClosedRange(0, 50, 10),
         hoursDivider = {
             Text(
@@ -140,18 +114,20 @@ private fun HoursNumberPicker3() {
                 textAlign = TextAlign.Center,
                 text = "m"
             )
-        }
+        },
+        textStyle = TextStyle(Color.White)
     )
 }
 
+@Preview
 @Composable
-private fun HoursNumberPicker4() {
+private fun HoursNumberPicker4Preview() {
     var state by remember { mutableStateOf<Hours>(FullHours(11, 36)) }
     HoursNumberPicker(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 16.dp),        leadingZero = true,
-
+            .padding(vertical = 16.dp),
+        leadingZero = true,
         value = state,
         onValueChange = {
             state = it
@@ -169,12 +145,14 @@ private fun HoursNumberPicker4() {
                 textAlign = TextAlign.Center,
                 text = "minutes"
             )
-        }
+        },
+        textStyle = TextStyle(Color.White)
     )
 }
 
+@Preview
 @Composable
-private fun DoublesPicker() {
+private fun DoublesPickerPreview() {
     val possibleValues = generateSequence(0.5f, { it + 0.25f })
         .takeWhile { it <= 5f }
         .toList()
@@ -183,12 +161,14 @@ private fun DoublesPicker() {
         label = { it.toString() },
         value = state,
         onValueChange = { state = it },
-        list = possibleValues
+        list = possibleValues,
+        textStyle = TextStyle(Color.White)
     )
 }
 
+@Preview
 @Composable
-private fun FruitPicker() {
+private fun FruitPickerPreview() {
     val possibleValues = listOf("🍎", "🍊", "🍉", "🥭", "🍈", "🍇", "🍍")
     var state by remember { mutableStateOf(possibleValues[0]) }
     ListItemPicker(
@@ -199,14 +179,16 @@ private fun FruitPicker() {
     )
 }
 
+@Preview
 @Composable
-private fun IntRangePicker() {
+private fun IntRangePickerPreview() {
     val possibleValues = (-5..10).toList()
     var value by remember { mutableStateOf(possibleValues[0]) }
     ListItemPicker(
         label = { it.toString() },
         value = value,
         onValueChange = { value = it },
-        list = possibleValues
+        list = possibleValues,
+        textStyle = TextStyle(Color.White)
     )
 }

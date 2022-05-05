a40c32e57ff690738098cdeaf8a7c6c5e310f585
Raphaël <raphael@chargemap.com>
2022-05-05 14:58:35 +0200

    1.0.5

--- publish.sh
+++ publish.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+VERSION_NAME="$1"
+VERSION_CODE="${VERSION_NAME//./}"
+
+sed -e "14s/.*/VERSION_NAME=$VERSION_NAME/" -i '' gradle.properties
+sed -e "15s/.*/VERSION_CODE=$VERSION_CODE/" -i '' gradle.properties
+
 git add .
 git commit -m "$1"
 git tag -a $1 -m "$1"
@@ -8,4 +14,3 @@ git push --follow-tags
 ./gradlew clean
 ./gradlew build
 ./gradlew uploadArchives --no-daemon --no-parallel
-#./gradlew closeAndReleaseRepository
